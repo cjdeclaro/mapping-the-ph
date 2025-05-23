ae8f8d01fa0b77da45f90b75f0730dc1c0f6fe0b
cjdeclaro <cjdeclaro16@gmail.com>
2025-05-23 18:06:50 +0800

    Add Magic 12 for senators

--- elections-2025/js/mapping.js
+++ elections-2025/js/mapping.js
@@ -59,7 +59,19 @@ function createFeatureEvents(feature, layer, category) {
   if (category == "averageVoterTurnOut") {
     tooltipText = `${name}${voterTurnOut ? `: ${voterTurnOut}%` : ''}`;
   } else {
-    tooltipText = `${name}${winnerName ? `: ${winnerName} ${winnerVotes} votes` : ''}`;
+    const magic12 = voteData?.voteTally?.[category]?.slice(0, 12);
+    tooltipText = `<strong>${name}</strong><br><br>`;
+
+    magic12.forEach((v, index) => {
+      const senatorName = v.name || "Unknown";
+      const voteCount = v.votes || 0;
+
+      if (index === 0) {
+        tooltipText += `<strong>${senatorName}: ${voteCount} votes</strong><br>`;
+      } else {
+        tooltipText += `${senatorName}: ${voteCount} votes<br>`;
+      }
+    });
   }
 
   layer.on({
